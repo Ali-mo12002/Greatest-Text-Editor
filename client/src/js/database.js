97191e66ec5b208ea82f079b6596b02b5e0cc434
Ali <iodk123@outlook.com>
2024-06-18 04:24:35 +0100

    got rid of debug comments

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,7 +14,7 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.log('PUT to the jateDB');
+  console.log('content updated');
   const jateDb = await openDB('jate', 2);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
@@ -29,13 +29,10 @@ export const putDb = async (content) => {
 }
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  console.log(' code content from the database');
-  console.log("open");
-
-  console.log("open");
+  console.log(' content fetched');
+  
 
   const jateDb = await openDB('jate', 2);
-  console.log("open");
   const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
   const request = store.get(2);
